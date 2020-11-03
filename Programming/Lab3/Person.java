0bc7b3651df8c8fc0ad3d452f18feab096e79215
Pavel Danilov <71098930+47iq@users.noreply.github.com>
2020-11-03 18:14:09 +0300

    Update Person.java

--- Programming/Lab3/Person.java
+++ Programming/Lab3/Person.java
@@ -1,6 +1,6 @@
 package com.company;
 
-abstract public class Person implements Saying, HavingHumanStats{
+abstract public class Person implements iPerson{
     private String name;
     private PersonTypes type;
 
