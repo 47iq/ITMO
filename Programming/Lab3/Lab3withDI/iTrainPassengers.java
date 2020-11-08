7c526c4f7214da8e1449369eb519f8bd52990bba
Pavel Danilov <71098930+47iq@users.noreply.github.com>
2020-11-08 22:08:05 +0300

    Delete iTrainPassengers.java

--- Programming/Lab3/Lab3withDI/iTrainPassengers.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-import java.util.ArrayList;
-
-public interface iTrainPassengers {
-    void addPassenger(Passenger passenger);
-    void removePassenger(Passenger passenger);
-    ArrayList<Passenger> getPassengers();
-}
